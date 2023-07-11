99380add055c5c12a71a784c53bd5d4d85fe0afc
Gustavo <gustavoborges832@gmail.com>
2023-07-10 23:14:40 -0300

    adding methods baseResourceService

---
+++ src/app/pages/entries/shared/services/base-resource.service.ts
@@ -0,0 +1,65 @@
+import { BaseResourceModel } from '../models/base-resource.model';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+export abstract class BaseResourceService<T extends BaseResourceModel> {
+  protected http: HttpClient;
+
+  constructor(protected apiPath: string) {}
+
+  getAll(): Observable<T[]> {
+    return this.http
+      .get(this.apiPath)
+      .pipe(catchError(this.handleError), map(this.jsonDataToResources));
+  }
+
+  getById(id: number): Observable<T> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http
+      .get(url)
+      .pipe(catchError(this.handleError), map(this.jsonDataToResource));
+  }
+
+  create(resource: T): Observable<T> {
+    return this.http.post(this.apiPath, resource).pipe(
+      catchError(this.handleError),
+      map((resource) => resource)
+    );
+  }
+
+  update(resource: T): Observable<T> {
+    const url = `${this.apiPath}/${resource.id}`;
+    return this.http.put(url, resource).pipe(
+      catchError(this.handleError),
+      map((resource) => resource)
+    );
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  // Protected methods
+
+  protected jsonDataToResources(jsonData: any[]): T[] {
+    const resources: T[] = [];
+    jsonData.forEach((element) => resources.push(element as T));
+    return resources;
+    // "as" = operador de conversão de tipo,
+    // Por exemplo, se você tem uma variável de tipo "any" e deseja convertê-la para um tipo mais específico, pode usar "as" para realizar a conversão
+  }
+
+  protected jsonDataToResource(jsonData: any): T {
+    return jsonData as T;
+  }
+
+  protected handleError(error: any): Observable<any> {
+    console.log('ERRO NA REQUISIÇÃO =>', error);
+    return throwError(() => error);
+  }
+}
