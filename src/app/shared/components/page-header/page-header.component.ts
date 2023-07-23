55593c4f1c339bc5fbd054e4805c4ab5f37adb50
Gustavo <gustavoborges832@gmail.com>
2023-07-22 21:00:00 -0300

    adding inputs

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.css']
 })
-export class PageHeaderComponent {
+export class PageHeaderComponent implements OnInit {
+
+  @Input() pageTitle: string;
+  @Input() buttonClass: string;
+  @Input() buttonText: string;
+  @Input() buttonLink: string;
+
+  constructor(){}
+
+  ngOnInit(): void {
+   
+    
+  }
 
 }
