c687fdf07e263d297969b16f55fefcf7b8cd2bd8
Gustavo <gustavoborges832@gmail.com>
2023-07-31 21:01:25 -0300

    adding config chart

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -6,6 +6,7 @@ import { CategoryService } from '../../categories/shared/category.service';
 import { Entry } from '../../entries/shared/entry.model';
 import { EntryService } from '../../entries/shared/entry.service';
 
+import currencyFormatter from 'currency-formatter';
 
 
 @Component({
@@ -51,6 +52,57 @@ export class ReportsComponent implements OnInit {
     if (!month || !year)
       alert('Você precisa selecionar um Mês e o Ano para gerar os relatórios');
     else
-      this.entryService.getByMonthAndYear(month, year);
+      this.entryService.getByMonthAndYear(month, year).subscribe(this.setValues.bind(this));
+  }
+
+  private setValues(entries: Entry[]) {
+    this.entries = entries;
+    this.calculateBalance();
+    this.setChartData();
+  }
+
+  private calculateBalance() {
+    let expenseTotal = 0;
+    let revenueTotal = 0;
+
+    this.entries.forEach(entry => {
+      if (entry.type === 'revenue') {
+        revenueTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL'});
+      } else {
+        expenseTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL'});
+      }
+    });
+
+    this.expenseTotal = currencyFormatter.format(expenseTotal, { code: 'BRL'});
+    this.revenueTotal = currencyFormatter.format(revenueTotal, { code: 'BRL'});
+    this.balance = currencyFormatter.format(revenueTotal - expenseTotal, { code: 'BRL'});
+  }
+
+  private setChartData() {
+    const chartData = [];
+
+    this.categories.forEach(category => {
+      const filteredEntries = this.entries.filter(entry => (entry.categoryId === category.id && entry.type === 'revenue'));
+
+      if (filteredEntries.length > 0) {
+        const totalAmount = filteredEntries.reduce((total, entry) => total + currencyFormatter.unformat(entry.amount, { code: 'BRL'}, 0)); 
+        
+        chartData.push({
+          categoryName: category.name,
+          totalAmount: totalAmount 
+        });
+      }
+    });
+
+    this.revenueChartData = {
+      labels: chartData.map(item => item.categoryName),
+      datasets: [
+        {
+          label: 'Gráficos de Receitas',
+          backgroundColor: '#9CCC65',
+          data: chartData.map(item => item.totalAmount)
+        }
+      ]
+    };
   }
 }
